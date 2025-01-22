3f1cec08340da0deb7200c6ddbe2898efe1b2e6b
Prathamesh Madde <maddeprathamesh2003@gmail.com>
2025-01-22 19:24:08 +0530

    new files added

---
+++ 5/basic.java
@@ -0,0 +1,13 @@
+public class basic {
+    public static void main (String args[]){
+     
+    int a = 1;
+    int b = 31;
+    String name = "PM";
+    System.out.println(a);
+    System.out.println(b);
+    System.out.println(name);
+
+
+    }
+}
---
+++ 5/sum.java
@@ -0,0 +1,11 @@
+public class sum {
+    public static void main (String arg[]){
+
+        int a=10 ;
+        int b=20;
+        int sum= a + b;
+        
+        System.out.println(sum);
+
+    }
+}
