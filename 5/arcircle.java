50454d5dd5b9ca1aa8c183b536f4b66d6d0a9737
Prathamesh Madde <maddeprathamesh2003@gmail.com>
2025-01-22 21:27:11 +0530

    Area of circle .

---
+++ 5/arcircle.java
@@ -0,0 +1,14 @@
+//Area of circle .
+import java.util.*;
+
+public class arcircle {
+    public static void main (String arg[]){
+
+        double pi= 3.14;
+        Scanner sc = new Scanner(System.in);
+        int r = sc.nextInt();
+        double area = pi * r * r ;
+        System.out.println(area); 
+        
+    }   
+}
