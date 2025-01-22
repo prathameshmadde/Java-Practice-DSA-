999b0348d4147c585198a0c459537595c73ea92f
Prathamesh Madde <maddeprathamesh2003@gmail.com>
2025-01-22 21:19:05 +0530

    Product of a and b

---
+++ 5/input.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class input {
+    
+    public static void main(String arg[]){
+        
+        Scanner scan = new Scanner (System.in);
+        String input = scan.next();
+        System.out.println(input);
+    }
+}
---
+++ 5/product.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class product {
+    public static void main(String arg[]){
+
+        Scanner scan = new Scanner (System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int prod = a*b;
+        System.out.println(prod);
+    }
+}
