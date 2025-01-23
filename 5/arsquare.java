a5f49e88ed17adfb93a3cf4194eee2bfd125715c
Prathamesh Madde <maddeprathamesh2003@gmail.com>
2025-01-23 10:37:38 +0530

    Area of Square .

---
+++ 5/arsquare.java
@@ -0,0 +1,13 @@
+//Area of square .
+import java.util.*;
+
+public class arsquare {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter side = ");
+        int side = sc.nextInt();
+        int area = side*side  ;
+        System.out.println("area of square = "+area);
+    }
+}
