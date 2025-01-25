441ce64fe435a2b70c0bbd9804189b3992863eae
Prathamesh Madde <maddeprathamesh2003@gmail.com>
2025-01-25 11:42:46 +0530

    even or odd

---
+++ 7 conditional/evenodd.java
@@ -0,0 +1,13 @@
+//Find even or odd .
+public class evenodd {
+    public static void main(String args[]){
+        int a =30;
+        if (a%2==0){
+            System.out.println(a+" is even.");
+        }
+        else{
+            System.out.println(a+" is odd.");
+        }
+    }
+    
+}
