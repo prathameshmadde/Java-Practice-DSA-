6c6acd2986658e9dab8f9a2ae6a73112cbdb6852
Prathamesh Madde <maddeprathamesh2003@gmail.com>
2025-01-26 01:37:52 +0530

    Calculator using switch .

---
+++ 7 conditional/calculator.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class calculator {
+    public static void main(String args[]){
+        Scanner sc = new Scanner (System.in);
+        
+        System.out.println("Enter value of a = ");
+        int a = sc.nextInt();
+        System.out.println("Enter Sign Operato = ");
+        char sign = sc.next().charAt(0);
+        System.out.println("Enter value of b = ");
+        int b = sc.nextInt();
+        int ans=0;
+        switch(sign){
+            case '+' :ans= a+b;
+                      break;
+            case '-': ans= a-b;
+            break; 
+            case '*': ans= a*b;
+            break;
+            case '%': ans= a%b;
+            break; 
+            case '/': ans= a/b;
+
+        }
+        System.out.println(ans);
+
+  
+    }
+}
