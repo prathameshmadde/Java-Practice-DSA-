213c93435c01a91969cad0a7a634f11174690cd3
Prathamesh Madde <maddeprathamesh2003@gmail.com>
2025-01-26 01:49:27 +0530

    check input no. is positive or negative .

---
+++ 7 conditional/positive.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+import javax.sound.midi.SysexMessage;
+
+public class positive {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter value = ");
+        int a = sc.nextInt();
+        if(a>=0){
+            System.out.println("a is positve no.");
+        }
+        else {
+            System.out.println("a is negative no . ");
+        }
+
+    }
+}
