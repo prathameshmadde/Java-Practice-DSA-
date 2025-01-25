44064854abb8088affe9dce95b90539fbd883362
Prathamesh Madde <maddeprathamesh2003@gmail.com>
2025-01-26 01:57:06 +0530

    Check you have fever or not .

---
+++ 7 conditional/fever.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class fever {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Temperature = ");
+        double temp =sc.nextDouble();
+        if (temp > 100){
+            System.out.println("You have Fever.");
+        }
+        else {
+            System.out.println("You dont have Fever.");
+        }
+    
+    }
+}
