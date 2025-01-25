cdc0696c9604cb2956085042e0951168a60629b9
Prathamesh Madde <maddeprathamesh2003@gmail.com>
2025-01-25 12:10:03 +0530

    Calculate Income tax.

---
+++ income.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class income {
+    public static void main(String args[]){
+        Scanner sc = new Scanner (System.in);
+        System.out.println("Enter Your income = ");
+        int income=sc.nextInt();
+        double tax1 =0;
+        if(income<500000){
+            System.out.println("No income Tax");
+        }
+        else if (income>= 500000 && income<= 1000000 ){
+            tax1 = income +income*(0.20);
+            
+        }
+        else {
+            tax1 = income +income*(0.30);
+            
+        }
+    System.out.println("Income tax  = "+ tax1);
+        
+   }
+}
