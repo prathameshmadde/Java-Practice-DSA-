01546180eb3c3ad237bdc2cfc2bc751ab442fd2a
Prathamesh Madde <maddeprathamesh2003@gmail.com>
2025-01-25 12:33:19 +0530

    Ternary Operator

---
+++ 7 conditional/ternary1.java
@@ -0,0 +1,8 @@
+public class Ternary1 {
+
+    public static void main(String[] args) {
+        int num = 10; // Hardcoded value
+        String typ = ((num % 2) == 0) ? "The number is even." : "The number is odd.";
+        System.out.println(typ);
+    }
+}
