2aceba37c03c96141f85b30e5fda859ec3fc2d42
Prathamesh Madde <maddeprathamesh2003@gmail.com>
2025-01-26 16:23:49 +0530

    Print no. form 1 to 10 using while loop.

---
+++ 8 Loops/number.java
@@ -0,0 +1,10 @@
+//Print no. form 1 to 10.
+public class number{
+    public static void main(String[] args) {
+        int count=1;
+        while(count<=10){
+            System.out.println(count);
+            count++;
+        }
+    }
+}
