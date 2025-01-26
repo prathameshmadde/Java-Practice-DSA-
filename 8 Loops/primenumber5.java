80d970e7b4ebde9a5dcdf15e1f0d70d484d15bfc
Prathamesh Madde <maddeprathamesh2003@gmail.com>
2025-01-26 21:30:52 +0530

    check no is prime or not .

---
+++ 8 Loops/primenumber5.java
@@ -0,0 +1,23 @@
+
+import java.util.*;
+public class primenumber5 {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter n =");
+        int n = sc.nextInt();
+        int answ=0;
+        for (int i=2; i<n;i++){
+
+            if (n%i==0){
+                answ=1;
+            }
+           
+            }
+            if (answ==1){
+                System.out.println("Not Prime .");
+            }
+            else{
+                System.out.println("Prime No.");
+            }
+        }
+    }
