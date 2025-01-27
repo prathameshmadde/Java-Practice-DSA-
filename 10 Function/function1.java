2c8ea3937a6c9d961596ab4a7830880717b2e653
Prathamesh Madde <maddeprathamesh2003@gmail.com>
2025-01-27 15:43:09 +0530

    Funtion to print helloworld.

---
+++ 10 Function/function1.java
@@ -0,0 +1,13 @@
+//Funtion to print helloworld.
+public class function1 {
+    
+    public static int printhelloworld() {   
+        System.out.println("Hello World");
+    return 0;
+    }
+
+    public static void main(String[] args) {
+        
+        printhelloworld();
+    }
+}
