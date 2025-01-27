f4ba4da586a19e754bc0a98e2f880b16366afe4c
Prathamesh Madde <maddeprathamesh2003@gmail.com>
2025-01-27 18:30:32 +0530

    Product of a and b "

---
+++ 10 Function/function3.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class function3 {
+    
+    public static int multiply(int x,int y){
+        int product = x*y;
+        return product;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a= sc.nextInt();
+        int b= sc.nextInt();
+        int product = multiply(a, b);
+        System.out.println("Product is = "+product);
+        sc.close();
+    }
+}
