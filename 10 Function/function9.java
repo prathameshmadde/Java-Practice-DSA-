aaa54f00dbd72ac5f916a70c57669519dd80d846
Prathamesh Madde <maddeprathamesh2003@gmail.com>
2025-01-30 17:01:16 +0530

    Decimal no. to Binary no.

---
+++ 10 Function/function9.java
@@ -0,0 +1,22 @@
+//Binary to Decimal no.
+public class function9 {
+    
+    public static void dectobinary(int n){
+        int bi =0;
+        
+        int pow=0;
+        while (n>0){
+        int r = n%2;
+        bi=bi+ (r* (int) Math.pow(10,pow));
+        n=n/2;
+        pow++;  
+        }
+        System.out.println(n +"Decimal "+"Binary no.="+bi);
+    }
+    public static void main(String[] args) {
+        
+        dectobinary(5);
+        
+    } 
+
+}
