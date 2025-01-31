9a4d1a158c7e71dab8e76de426990abed8d5f1d5
Prathamesh Madde <maddeprathamesh2003@gmail.com>
2025-01-31 17:11:59 +0530

    Floyd's Triangle.

--- 11 Pattern part-2/P3Pyramid.java
+++ 11 Pattern part-2/P3Pyramid.java
@@ -1,13 +1,15 @@
 // Floyd's Triangle.
-// 1
-// 23
-// 345
-// 4567
+//  1
+//  2 3
+//  4 5 6
+//  7 8 9 10
 public class P3Pyramid {
     public static void main(String[] args) {
+        int counter=1;
         for(int i=1;i<=4;i++){
             for(int j=1;j<=i;j++){
-                System.out.print(j+i-1);
+                System.out.print(" "+ counter);
+                counter++;
             }
             System.out.println();
         }
