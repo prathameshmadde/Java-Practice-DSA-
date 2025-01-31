81d2f0f9b250e55f7e633855ca9b774ff5176c41
Prathamesh Madde <maddeprathamesh2003@gmail.com>
2025-01-31 13:24:23 +0530

    Hollow Pattern.

--- 11 Pattern part-2/P1Hollow.java
+++ 11 Pattern part-2/P1Hollow.java
@@ -1,12 +1,12 @@
-// * * * *
-// *     *
-// *     *
-// * * * *
-public class P1 {
+// * * * * *
+// *       *
+// *       *
+// * * * * *
+public class P1Hollow {
     public static void main(String[] args) {
         for(int i=0;i<4;i++){
-            for(int j=0;j<4;j++){
-                if (i==0 || i==3 || j==0 || j==3){
+            for(int j=0;j<5;j++){
+                if (i==0 || i==3 || j==0 || j==4){
                     System.out.print(" *");
                 }
                 else{
