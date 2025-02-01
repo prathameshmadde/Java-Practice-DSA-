72b1f7d5a3d8e0f379f12835c0e0a4f9bf0fc0ff
Prathamesh Madde <maddeprathamesh2003@gmail.com>
2025-02-01 17:02:53 +0530

    Linear Search.

--- 12 Arrays/A2_LargestinArray.java
+++ 12 Arrays/A2_LargestinArray.java
@@ -1,20 +1,25 @@
 //Linear Search.
 public class A2_LargestinArray {
 
-    public static int largestvalue(int[] rollno,int largest){
+    public static void largestvalue(int[] rollno,int largest,int smallest){
             for(int i= 0; i< rollno.length; i++){
                 if (rollno[i] > largest){
-                largest = rollno[i];
+                   largest = rollno[i];
                 }
-        }
-        return largest;
+                if (rollno[i] < smallest){
+                    smallest = rollno[i];
+                }
+        }       
+        System.out.println("Largest vlaue = "+largest);
+        System.out.println("Smallest vlaue = "+smallest);
+        
     }
     
     public static void main(String[] args) {
         int rollno[]={1,5,3,8,2,9,7};
-
+        int smallest= Integer.MAX_VALUE;
         int largest = Integer.MIN_VALUE;
-        int answer =largestvalue(rollno,largest);
-        System.out.println("Largest no = "+answer);
+        largestvalue(rollno,largest,smallest);
+        
     }
 }
