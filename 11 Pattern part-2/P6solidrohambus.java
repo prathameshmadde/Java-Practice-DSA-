ebe9ad97165ed79612a13946ee3c378c19dfecd1
Prathamesh Madde <maddeprathamesh2003@gmail.com>
2025-02-01 14:22:20 +0530

    Solid Rohambus.

--- 11 Pattern part-2/P6solidrohambus.java
+++ 11 Pattern part-2/P6solidrohambus.java
@@ -4,18 +4,24 @@
 //   *****
 //  *****
 // *****
+import java.util.*;
 public class  P6solidrohambus {
-    public static void main(String[] args) {
-        
-        for (int i=1; i<=5; i++){
-            for (int j=4; j>=i; j--){
+
+    public static void roham(int n){
+        for (int i=1; i<=n; i++){
+            for (int j=n-1; j>=i; j--){
                 System.out.print(" ");
             }
-            for (int j=5; j>=1; j--){
+            for (int j=n; j>=1; j--){
                 System.out.print("*");
             }
             System.out.println("");
         }
-        
+    }
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+       System.out.println("Enter n = ");
+       int n=sc.nextInt();
+       roham(n);
     }
 }
