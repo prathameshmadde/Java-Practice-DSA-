8a2d893bf08cc831284a215e3321adaa1d273c29
Prathamesh Madde <maddeprathamesh2003@gmail.com>
2025-02-03 03:04:26 +0530

    Binary Search

--- 12 Arrays/A3_BinarySearch.java
+++ 12 Arrays/A3_BinarySearch.java
@@ -6,12 +6,12 @@ public class A3_BinarySearch {
         return (start + end)/2 ;
     }
 
-    public static void binerysearch(int numbers [],int key){
+    public static int  binerysearch(int numbers [],int key){
         int start = 0;
         int end = 6;
         int mid = midvalue(start, end);
-        int x= 1;
-        while (x!=0 ){
+        
+        while (start < end ){
         if(numbers[mid]>key){
            end = mid - 1;
            mid = midvalue(start,end);
@@ -19,17 +19,18 @@ public class A3_BinarySearch {
         else if (numbers[mid] <key){
            start = mid + 1;
            mid = midvalue(start,end);
+
         }
         else{
-            System.err.println("value at position " + mid);
-            x=0;
+             return mid;
         }
         } 
+        return -1;
     }
     
     public static void main(String[] args) {
         int numbers[]={2,4,6,8,10,12,14};
-        int key = 10;
-        binerysearch(numbers, key);
+        int key = 100;
+        System.out.println("Value found at = "+ binerysearch(numbers, key));
     }
 }
