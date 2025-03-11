901688ba51785ca6b4d70edbd1aade463d79619f
Prathamesh Madde <maddeprathamesh2003@gmail.com>
2025-03-11 16:17:40 +0530

    Diagonal Matrix

--- 15.2D Array/A4_DiagonalMatrix.java
+++ 15.2D Array/A4_DiagonalMatrix.java
@@ -4,15 +4,25 @@ class A4_DiagonalMatrix{
 
     public static int printDiagonal(int [][] matrix){
         int sum = 0 ;
-        for(int i=0; i<matrix.length; i++){
-            for(int j=0; j<matrix[i].length; j++){
-                if(i==j){
-                    sum+=matrix[i][j]; 
-                }
+        // for(int i=0; i<matrix.length; i++){
+        //     for(int j=0; j<matrix[i].length; j++){
+        //         if(i==j){
+        //             sum+=matrix[i][j]; 
+        //         }
 
-                else if (i+j == matrix.length-1){
-                    sum+=matrix[i][j]; 
-                }
+        //         else if (i+j == matrix.length-1){
+        //             sum+=matrix[i][j]; 
+        //         }
+        //     }
+        // }
+        
+        for (int i = 0; i < matrix.length; i++) {
+            
+            sum += matrix[i][i];
+
+            if ( i != (matrix[0].length - i - 1) )
+            {
+            sum += matrix[i][matrix.length - i - 1];
             }
         }
         return sum;
